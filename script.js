c5703597303064b2127b106960abe288c9d95651
BunkerStriker <130099468+BunkerStriker@users.noreply.github.com>
2023-10-15 23:15:10 +0300

    start

---
+++ script.js
@@ -0,0 +1,46 @@
+// Initialize Ace editor
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/twilight"); /* Use a dark theme for the editor */
+//editor.session.setMode("ace/mode/javascript");
+
+// Custom function to display output in the output area
+function writeToOutput(...args) {
+  var outputElement = document.getElementById("output");
+  var message = args.map(arg => String(arg)).join(" "); // Convert all arguments to strings and join them
+  outputElement.textContent += message + "\n"; // Append the message to the output area
+}
+// Custom function to clear the output area
+function clearOutput(...args) {
+  var outputElement = document.getElementById("output");
+  outputElement.textContent = "";
+}
+// Redirect console commands to the writeToOutput function
+console.log = writeToOutput;
+console.warn = writeToOutput;
+console.error = writeToOutput;
+
+// Function to run the code using eval
+function runCode() {
+  clearOutput();
+  var code = editor.getValue();
+  var outputElement = document.getElementById("output");
+
+  try {
+    var compiledCode = compileCode(code);
+    var result = eval(compiledCode);
+    //var result = eval(code);
+    writeToOutput(result); // Log the result to the output area using the custom function
+  } catch (error) {
+    writeToOutput("Error: " + error.message); // Log the error message to the output area using the custom function
+  }
+}
+
+function compileCode(input) {
+  const lines = input.toString().split("\n");
+  const processedLines = lines.map(line => line.replace("var", "let") + ";");
+  const result = processedLines.join("\n");
+  return result;
+}
+
+// Event listener for the Run button
+document.getElementById("run-btn").addEventListener("click", runCode);
